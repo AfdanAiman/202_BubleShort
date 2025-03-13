3ac5342caf63416b182bcb92fe7e0233a3f1b5db
AfdanAiman <afdanaiman06@gmail.com>
2025-03-13 11:14:23 +0700

    menambahkan prosedur display

--- file2.cpp
+++ file2.cpp
@@ -29,7 +29,7 @@ void input(){   //Proseur input
     }
 }
 
-void bubbleSortArray()
+void bubbleShortArray()
 {
     int pass = 1;   // step 1
 
@@ -46,3 +46,20 @@ void bubbleSortArray()
         pass = pass + 1; // step 4
     }while(pass <= n-1); // step 5
 }
+
+void display(){
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Element array telah tersusun" << endl;
+    cout << "=====================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1){
+            cout << "-->";
+        }
+    }
+}
+
+int main()
+    
