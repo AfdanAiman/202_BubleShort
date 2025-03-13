1b4a7b5ef985fc64273b7670d29574e3b95ae89e
AfdanAiman <afdanaiman06@gmail.com>
2025-03-13 11:27:12 +0700

    memanggil prosedur bubbleshort

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+void bubbleShortArray()
+{
+    int pass = 1;   // step 1
+
+    do {
+        for (int j=0; j<=n-1-pass; j++){   // step 2
+            if (arr[j] > arr[j+1]) {        // step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
